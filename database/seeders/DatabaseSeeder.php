d0725e2f3585a90c48a89ebc9d8c9c9a694fd866
samratconsultancyjnp-lgtm <277428030+samratconsultancyjnp-lgtm@users.noreply.github.com>
2026-04-28 15:12:51 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'admin@bhagva.org'],
+            [
+                'name'              => 'Admin',
+                'email'             => 'admin@bhagva.org',
+                'password'          => Hash::make('Admin@123'),
+                'is_admin'          => 1,
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
